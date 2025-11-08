f3353c01fd37527b20dc54f44f2fe344b2822630
S A N <sankhamazumder099@gmail.com>
2025-11-08 13:23:01 +0530

    First commit

--- Backend/src/models/User.Schema.js
+++ Backend/src/models/User.Schema.js
@@ -1,1 +1,45 @@
 import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    fullName : {
+        type:String,
+        required: true,
+        lowercase: true,
+        trim:true
+    },
+    email : {
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true,
+        index:true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: [true,"Password is required."]
+    },
+    avatar:{
+        type: String,
+        required: true
+    },
+    coverImage:{
+        type: String,
+        required: true
+    },
+    role:{
+        type:String,
+        enum:['user','admin','editor','premium'],
+        default:'user',
+        index:true
+    },
+    metaData:{
+        type:mongoose.Schema.Types.Mixed
+    },
+    refreshToken:{
+        type:String
+    },
+
+},{timestamps:true});
+
+export const User = mongoose.model("User",userSchema);
